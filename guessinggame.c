6e70ca5d0f15a57323931e6f0c652c3013eb2526
norivera <89235780+norivera@users.noreply.github.com>
2021-09-24 03:32:33 -0700

    intial commit formatting program

---
+++ guessinggame.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> 
+
+int main()
+{
+    //declare variables needed 
+
+//use switch statments for the menu 
+
+//switch 1: will be the regular base version of the game guessing between 1-10 
+
+//switch 2: will ask the user to change the max number a user can guess when playing 
+
+//switch 3: wil quit the game     
+}
+
