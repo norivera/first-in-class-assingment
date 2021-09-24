515b6c086bdddd0e0279ead0f5ac77e4674852c0
norivera <89235780+norivera@users.noreply.github.com>
2021-09-24 03:47:10 -0700

    added variables needed

--- guessinggame.c
+++ guessinggame.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <time.h> 
 
 int main()
 {
     //declare variables needed 
-
-//use switch statments for the menu 
-
-//switch 1: will be the regular base version of the game guessing between 1-10 
-
-//switch 2: will ask the user to change the max number a user can guess when playing 
-
-//switch 3: wil quit the game     
+    FILE *fp, *out;
+    time_t t;
+    srand((unsigned)time(&t));
+    int inputChoice = 0;
+    int maxValue = 10;
+    //use switch statments for the menu 
+    //switch 1: will be the regular base version of the game guessing between 1-10 
+    //switch 2: will ask the user to change the max number a user can guess when playing 
+    //switch 3: wil quit the game     
 }
 
