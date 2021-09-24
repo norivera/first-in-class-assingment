a639799354e35a480d6d585f66dc93d9899085fe
norivera <89235780+norivera@users.noreply.github.com>
2021-09-24 03:28:19 -0700

    complete program

--- restaurantbill.c
+++ restaurantbill.c
@@ -1,8 +1,45 @@
-//add neeeded libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> 
 
-//create main with command line preamiters
+int main(int argc, char *argv[]) 
+{
 
-//imipliment random gen to get numbers and mod 4 to get 0-3
+    time_t t;
+    srand((unsigned)time(&t));
+    int num = rand() % 4;
+    double price;
+    if (num == 0) 
+    {
+        price = 9.95;
+    }
+    if (num == 1) 
+    {
+        price = 4.55;
+    }
+    if (num == 2) 
+    {
+        price = 13.25;
+    }
+    if (num == 3) 
+    {
+        price = 22.35;
+    }
 
-//use if statments to input price 
-//caluclate everthing 
+    printf("Meal Cost: $%.2f \n", price);
+    double tax, tip;
+    sscanf(argv[1], "%lf", &tax);
+    sscanf(argv[2], "%lf", &tip);
+
+    tax = tax * price; 
+    printf("Tax: $%.2f\n", tax);
+    
+    tax = tax + price;
+    tip = tip * tax;
+    printf("Tip: $%.2f\n", tip);
+
+    price = tax + tip;
+    printf("Total Bill: $%.2f\n", price);
+    return 0;
+
+}
