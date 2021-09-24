de65bf64b0b68d30c08d45000465a66978210434
norivera <89235780+norivera@users.noreply.github.com>
2021-09-24 03:26:59 -0700

    Initial commit organizing program

---
+++ restaurantbill.c
@@ -0,0 +1,8 @@
+//add neeeded libraries
+
+//create main with command line preamiters
+
+//imipliment random gen to get numbers and mod 4 to get 0-3
+
+//use if statments to input price 
+//caluclate everthing 
