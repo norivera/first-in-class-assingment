7ce8de4d497464d7a1f0e0734a92e2e043e902e1
norivera <89235780+norivera@users.noreply.github.com>
2021-09-24 21:21:04 -0700

    made overall corrections

--- guessinggame.c
+++ guessinggame.c
@@ -66,6 +66,3 @@ int main()
         }
     }
 }
-    //switch 1: will be the regular base version of the game guessing between 1-10 
-    //switch 2: will ask the user to change the max number a user can guess when playing 
-    //switch 3: wil quit the game  
