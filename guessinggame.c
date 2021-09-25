085170992e65bb5490c14245e8d1c8d194fc180f
norivera <89235780+norivera@users.noreply.github.com>
2021-09-24 21:39:34 -0700

    fixed an error invovling case statements

--- guessinggame.c
+++ guessinggame.c
@@ -1,54 +1,53 @@
+   
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <time.h>
 
-int main()
-{
-    //varaible declaration
+int main(){
     FILE *fp, *out;
     time_t t;
     srand((unsigned) time(&t));
-    int inputChoice = 0;
-    int maxValue = 10;
 
-    //Menu creation 
-    while (inputChoice != 3) {
-        printf("\nEnter option:\n[1]:Guess number\n [2]:Change max\n [3]:Exit\n");
+    int inputChoice = 0;
+    //================================
+    int maxValue;
+    fp = fopen("data.txt","r");
+    fscanf(fp, "%d", &maxValue);
+    fclose(fp);
+    //================================
+    while (inputChoice != 3) 
+    {
+        printf("\nEnter a option: \n [1]:Guess a number \n [2]:Change max \n [3]:Exit\n");
         scanf("%d", &inputChoice);
-        switch (inputChoice)
-         {
+        switch (inputChoice) 
+        {
             case 1: ;
                 int num = rand() % maxValue+1;
                 bool inProgress = false;
-                printf("\nA random number has been generated, input number to start guessing: ");
-                while (!inProgress) 
-                {
-                    char userInput;
-                    scanf("%c", &userInput);
+                printf("\nA random number has been generated. Please choose a random number: ");
+                while (!inProgress)
+                 {
+                    char userInput[3];
+                    scanf("%s", &userInput);
 
-                    int convert = (int)userInput;
-                    if (convert > num+48 && convert != 10 && convert != 113)
+                    if (userInput[0] == 'q') 
                     {
-                        printf("Too high, try again!\n");
-                    } 
-                    else if (convert < num+48 && convert != 10 && convert != 113)
-                    {
-                        printf("Too low, try again!\n");
-                    }
-                    else if (convert == 10)
-                    {
-                        ;
-                    }
-                    else if (convert == 113)
-                    {
-                        printf("\nExiting the game, back to main menu!");
-                        inProgress = true;
-                    }
-                    else
-                    {
-                        printf("Correct!");
-                        inProgress = true;
+                        printf("Break out!");
+                        break;
+                    } else {
+                        int convert;
+                        sscanf(userInput, "%d", &convert);
+                        //printf("\nTest: %d", convert);
+
+                        if (convert > num) {
+                            printf("Too high, try again!\n");
+                        } else if (convert < num) {
+                            printf("Too low, try again!\n");
+                        } else {
+                            printf("Correct!\n");
+                            inProgress = true;
+                        }
                     }
                 }
                 break;
@@ -56,13 +55,21 @@ int main()
                 printf("\nWhat is the max number? ");
                 scanf("%d", &maxValue);
                 printf("Max number saved as: %d", maxValue);
+                //================================
+                out = fopen("data.txt", "w");
+                fprintf(out, "%d", maxValue);
+                fclose(out);
+                //================================
                 break;
             case 3:
                 printf("Thanks for playing!");
                 exit(0);
             default:
-                printf("invalid option");
+                printf("invalid option\n");
                 break;
         }
     }
 }
+    //switch 1: will be the regular base version of the game guessing between 1-10 
+    //switch 2: will ask the user to change the max number a user can guess when playing 
+    //switch 3: wil quit the game
